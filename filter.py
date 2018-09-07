f98da25b9804b403d981d2fdcf24df313e8935d6
Bondsam1 <34954783+Bondsam1@users.noreply.github.com>
2018-09-07 21:44:52 +0800

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,15 @@
+with open('filtered_words.txt','r') as f:
+    text=f.read()
+word=text.split('\n')
+judge=True
+while(1):
+    k=input('enter to test:')
+    for i in word:
+        if(k==i):
+            judge=False
+    if(judge==False):
+        print('ブーブーですわ')
+    else:
+        print('OK')
+    judge=True
+
